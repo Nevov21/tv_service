b5381f64f24d6ec1ba94afba749bd9070632e9c1
Nevov21 <jaceszyk@gmail.com>
2025-12-14 21:08:28 +0100

    Dodanie logowania

--- tv/tv.py
+++ tv/tv.py
@@ -1,13 +1,34 @@
-from flask import Flask, request, render_template, redirect, url_for, jsonify
+from flask import Flask, request, render_template, redirect, url_for, jsonify, Response
 import subprocess
 import streamlink
+from functools import wraps
 
 app = Flask(__name__)
+
+def check_auth(username, password):
+    """Sprawdza poprawność loginu i hasła."""
+    return username == 'belpaese' and password == '777777'
+
+def authenticate():
+    """Wysyła nagłówek 401 żądający autoryzacji."""
+    return Response(
+    'Musisz się zalogować, aby uzyskać dostęp.', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 player_process = None
 PLAYER_CMD = 'mpv --fs --no-border --ao=alsa --audio-device=hdmi'
 
 @app.route("/get_qualities", methods=["POST"])
+@requires_auth
 def get_qualities():
     url = request.form.get("url")
     if not url:
@@ -20,10 +41,12 @@ def get_qualities():
         return jsonify({"error": str(e)}), 500
 
 @app.route("/", methods=["GET"])
+@requires_auth
 def index():
     return render_template("index.html")
 
 @app.route("/play", methods=["POST"])
+@requires_auth
 def play():
     global player_process
 
@@ -49,6 +72,7 @@ def play():
     return redirect(url_for("index"))
 
 @app.route("/stop", methods=["POST"])
+@requires_auth
 def stop():
     stop_player()
     return redirect(url_for("index"))
